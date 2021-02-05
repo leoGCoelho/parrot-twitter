5006424b59570eb94283a7ecdda842c692c2a6b1
Leonardo Gularte Coelho <leo_coelho_@hotmail.com>
2021-02-05 08:39:22 -0300

    issues fixed

--- defaultfunc.py
+++ defaultfunc.py
@@ -8,32 +8,6 @@ except:
     import snscrape.modules.twitter as sntwitter
 
 
-#def FormatTime(dma, hms):
-def FormatTime(dma):
-    now = datetime.datetime.now()
-    formated = ""
-
-    if dma == "":
-        formated.append(str(now.year) + "-" + str(now.month) + "-" + str(now.day))
-    else:
-        dmaV = dma.split("/")
-        if len(dmaV) == 0:
-            dmaV = []
-            dmaV.append(str(now.day))
-        if len(dmaV) == 1:
-            dmaV.append(str(now.month))
-        if len(dmaV) == 2:
-            dmaV.append(str(now.year))
-
-        '''hmsV = hms.split(":")
-        if len(hmsV) == 2:
-            hmsV.append("00")'''
-
-        formated = dmaV[2] + "-" + dmaV[1] + "-" + dmaV[0]
-
-    #print(formated)
-    return formated
-
 def ExtractData(lim, date, usern, hashtag):
     tweetlist = []
 
@@ -56,3 +30,85 @@ def ExtractData(lim, date, usern, hashtag):
         tweetlist.append([tweet.date, tweet.content])
 
     return tweetlist
+
+
+def GetCurrDate():
+    now = datetime.datetime.now()
+    return (str(now.year) + "-" + str(now.month) + "-" + str(now.day))
+
+
+def GetNPassDate(days):
+    passday = datetime.datetime.now() - datetime.timedelta(days)
+    return (str(passday.year) + "-" + str(passday.month) + "-" + str(passday.day))
+
+
+def GetDatetime(date):
+    if (date == 'Agora') or (date == 'agora') or (date == 'AGORA'):
+        return GetCurrDate()
+    elif (date == 'Hoje') or (date == 'hoje') or (date == 'HOJE') or (date == 'hj') or (date == 'HJ'):
+        return GetCurrDate()
+    elif (date == 'nesse momento') or (date == 'Nesse momento') or (date == 'NESSE momento') or (date == 'nesse Momento') or (date == 'nesse MOMENTO') or (date == 'NESSE momento') or (date == 'Nesse Momento') or (date == 'NESSE MOMENTO'):
+        return GetCurrDate()
+    elif (date == 'neste momento') or (date == 'Neste momento') or (date == 'NESTE momento') or (date == 'neste Momento') or (date == 'neste MOMENTO') or (date == 'NESTE momento') or (date == 'Neste Momento') or (date == 'NESTE MOMENTO'):
+        return GetCurrDate()
+
+    elif (date == 'Ontem') or (date == 'ontem') or (date == 'ONTEM'):
+        return GetNPassDate(1)
+    elif (date == '1 dia') or (date == 'um dia') or (date == '1 Dia') or (date == 'Um Dia') or  (date == '1 DIA') or (date == 'UM DIA'):
+        return GetNPassDate(1) 
+
+
+    elif (date == 'Anteontem') or (date == 'anteontem') or (date == 'ANTEONTEM'):
+        return GetNPassDate(2)
+    elif (date == '2 dias') or (date == 'dois dias') or (date == '2 Dias') or (date == 'Dois Dias') or  (date == '2 DIAS') or (date == 'DOIS DIAS'):
+        return GetNPassDate(2)
+
+    else:
+        return FormatTime(date)
+
+    return ""
+
+
+#def FormatTime(dma, hms):
+def FormatTime(dma):
+    now = datetime.datetime.now()
+    formated = ""
+
+    if dma == "":
+        formated.append( GetCurrDate() )
+    else:
+        dmaV = str(dma).split("/")
+        if len(dmaV) == 0:
+            dmaV = []
+            dmaV.append(str(now.day))
+        if len(dmaV) == 1:
+            dmaV.append(str(now.month))
+        if len(dmaV) == 2:
+            dmaV.append(str(now.year))
+
+        '''hmsV = hms.split(":")
+        if len(hmsV) == 2:
+            hmsV.append("00")'''
+
+        formated = dmaV[2] + "-" + dmaV[1] + "-" + dmaV[0]
+
+    #print(formated)
+    return formated
+
+
+def SetDatetime(dma):
+    data = []
+    #print(dma)
+
+    if dma == []:
+        data = [GetCurrDate(), GetCurrDate()]
+    else:
+        for date in dma:
+            data.append( GetDatetime(date) )
+
+        if len(dma) == 1:
+            data.append( GetCurrDate() )
+
+    return data
+
+            
--- main.py
+++ main.py
@@ -34,66 +34,48 @@ def main():
     date = []
     data = request.get_json(silent=True)
 
-    contexts = data['queryResult']['outputContexts']
-    for context in contexts:
-        parametros = context['parameters']
-        users = parametros['username']
-        hashtags = parametros['hashtag']
-        quantidade = parametros['number-integer']
-        periodo = parametros['datetime_exp']
-        dma = parametros['datetime_dma']
-        hms = parametros['datetime-hms']
+    try:
+        contexts = data['queryResult']['outputContexts']
+        for context in contexts:
+            parametros = context['parameters']
+            users = parametros['username']
+            hashtags = parametros['hashtag']
+            quantidade = parametros['number-integer']
+            dma = parametros['datetime_dma']
+            hms = parametros['datetime-hms']
 
-    #users[0] = re.sub(r'@', r'', users[0])
-    print(users)
-    print(hashtags)
-    print(quantidade)
-    print(periodo)
-    print(dma)
-    print(hms)
+        #users[0] = re.sub(r'@', r'', users[0])
+        print(users)
+        print(hashtags)
+        print(quantidade)
+        print(dma)
+        print(hms)
 
-    if len(quantidade) == 0:
-        lim = 1
-    for quant in quantidade:
-        lim = int(quant)
+        if len(quantidade) == 0:
+            lim = 1
+        for quant in quantidade:
+            lim = int(quant)
 
-    print(lim)
+        #print(lim)
 
-    for dmaV in dma:
-        date.append(func.FormatTime(dmaV))
-    if len(dmaV) == 0:
-        date = [func.FormatTime("03/02/2021"), func.FormatTime("03/02/2021")]
-    elif len(dmaV) == 1:
-        date.append(func.FormatTime("03/02/2021"))
-    txt = ""
+        date = func.SetDatetime(dma)
+        print(date)
+        txt = ""
 
-    '''if len(hashtags) == 0:
-        if len(users) == 0:
-            extrated = func.ExtractData(lim, date, "", "")
-            txt += "O tweet de" + extrated[2] + ' é:\n\n' + extracted[1] + '\n\nPosted in ' + extracted[0] + "\n\n"
-        else:
-            for user in users:
-                extrated = func.ExtractData(lim, date, user, "")
-                    txt += "O tweet de" + extrated[2] + ' é:\n\n' + extracted[1] + '\n\nPosted in ' + extracted[0] + "\n\n"
-    else:
-        for hashtag in hashtags:
-            if len(users) == 0:
-                extrated = func.ExtractData(lim, date, "", hashtag)
-                txt += "O tweet de" + extrated[2] + ' é:\n\n' + extracted[1] + '\n\nPosted in ' + extracted[0] + "\n\n"
-            else:
-                for user in users:
-                    extrated = func.ExtractData(lim, date, user, hashtags)
-                    txt += "O tweet de" + extrated[2] + ' é:\n\n' + extracted[1] + '\n\nPosted in ' + extracted[0] + "\n\n"'''
+        if (users == '') and (hashtags == ''):
+            users = "@bbb"
 
-    extracted = func.ExtractData(lim, date, "", hashtags[0])
-    
-    for extr in extracted:
-        txt += "O tweet de" + ' é:\n\n' + str(extr[1]) + '\n\nPostado em ' + str(extr[0]) + "\n\n"
-        #txt = str(extrated)
+        extracted = func.ExtractData(lim, date, users, hashtags)
+        
+        for extr in extracted:
+            txt += "O tweet de" + ' é:\n\n' + str(extr[1]) + '\n\nPostado em ' + str(extr[0]) + "\n\n"
+            print(txt)
+            #txt = str(extrated)
 
-    data['fulfillmentText'] = txt
+        data['fulfillmentText'] = txt
 
-    #print(data)
+    except:
+        print(data)
 
     return jsonify(data)
 
